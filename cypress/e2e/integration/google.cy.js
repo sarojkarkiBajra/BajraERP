c9edfdc95a442805946fa20251f8db28db65055e
sarojkarkiBajra <107462270+sarojkarkiBajra@users.noreply.github.com>
2022-06-14 13:12:53 +0545

    changed file

--- cypress/e2e/integration/google.cy.js
+++ cypress/e2e/integration/google.cy.js
@@ -8,13 +8,6 @@ describe(['my-feature'],'Bajra ERP Test Suite', () => {
       cy.get('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b').click()
 
     })
-    it(['sip'],'Movie search in google', () => {
-      cy.visit('https://www.google.com')
-      cy.get('[class="gLFyf gsfi"]').type('Movie')
-      cy.get('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b').click()
-      cy.get('[href="https://imdb.com/"] > .LC20lb').click()
-      
-    })
   
     })
   
